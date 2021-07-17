9f988edd1f7e89d1b858b6bbeca96c7de554e825
Kazuya Ueoka <info@fromkk.info>
2021-07-17 18:39:54 +0900

    install SwiftGen

--- AccessibilitySampler/ViewController.swift
+++ AccessibilitySampler/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = L10n.title
         // Do any additional setup after loading the view.
     }
 
